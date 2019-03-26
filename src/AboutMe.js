80616b211f21da578f02ab07490ccf8e99be24de
Ken Snow <k.snowjr@gmail.com>
2019-03-26 11:15:16 -0600

    updated links

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -10,7 +10,7 @@ function AboutMe() {
                 <Link to='/contact'>Contact Me</Link>
                 <p>I am a full stack web developer in the MERN stack.  I enjoy new challenges and languages, and have a background with data visualization, script writing, and parsing.  I am currently seeking employment as a web developer.</p>
                 <p>I am located in the City of Salt, UT, and I am happy to discuss to both local and remote project or employment opportunities.</p>
-                <p>This page was created using React. Source code can be found <a href="http://github.com/kensnow/projects/tree/master/portfolio-project">here</a></p>
+                <p>This page was created using React. Source code can be found <a href="https://github.com/kensnow/portfolio-site">here</a></p>
 
             </div>
 
